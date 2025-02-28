7b75a6ca1c1214f0a1ceccf6b33fe45c17cc53a9
mollydougher <118477334+mollydougher@users.noreply.github.com>
2025-02-28 10:33:58 -0500

    non-dimensionalize length scale

--- src/prommis/nanofiltration/high_fidelity_membrane_single_salt.py
+++ src/prommis/nanofiltration/high_fidelity_membrane_single_salt.py
@@ -23,12 +23,13 @@ import matplotlib.pyplot as plt
 
 def main():
     m = build_model()
-    discretize_model(m, NFEx=6, NFEz=4)
+    discretize_model(m, NFEx=10, NFEz=5, discretization="findif")
     dt = DiagnosticsToolbox(m)
-    dt.report_structural_issues()
+    dt.assert_no_structural_warnings()
 
     solve_model(m)
-    dt.assert_no_numerical_warnings()
+    # dt.assert_no_numerical_warnings()
+    dt.report_numerical_issues()
 
     # svd = dt.prepare_svd_toolbox()
     # svd.display_rank_of_equality_constraints()
@@ -145,8 +146,10 @@ def build_model():
     )
 
     # define length scales
-    m.x = ContinuousSet(bounds=(0, value(m.w)))
-    m.z = ContinuousSet(bounds=(0, value(m.l)))
+    # m.x = ContinuousSet(bounds=(0, value(m.w)))
+    m.x_bar = ContinuousSet(bounds=(0,1))
+    # m.z = ContinuousSet(bounds=(0, value(m.l)))
+    m.z_bar = ContinuousSet(bounds=(0,1))
 
     # define remaining algebraic variables
     ## independent of x,z length scale
@@ -167,70 +170,70 @@ def build_model():
 
     ## dependent on x
     m.volume_flux_water = Var(
-        m.x,
+        m.x_bar,
         initialize=0.03,
         units=units.m**3 / units.m**2 / units.h,
         domain=NonNegativeReals,
         doc="Volumetric water flux of water across the membrane",
     )
     m.mass_flux_lithium = Var(
-        m.x,
+        m.x_bar,
         initialize=0.05,  # TODO: verify good value
         units=units.kg / units.m**2 / units.h,
         domain=NonNegativeReals,
         doc="Mass flux of lithium across the membrane (z-direction, x-dependent)",
     )
     m.mass_flux_chlorine = Var(
-        m.x,
+        m.x_bar,
         initialize=0.05,  # TODO: verify good value
         units=units.kg / units.m**2 / units.h,
         domain=NonNegativeReals,
         doc="Mass flux of chlorine across the membrane (z-direction, x-dependent)",
     )
     m.retentate_flow_volume = Var(
-        m.x,
+        m.x_bar,
         initialize=130,
         units=units.m**3 / units.h,
         domain=NonNegativeReals,
         doc="Volumetric flow rate of the retentate, x-dependent",
     )
     m.retentate_conc_mass_lithium = Var(
-        m.x,
+        m.x_bar,
         initialize=1.33,
         units=units.kg / units.m**3,
         domain=NonNegativeReals,
         doc="Mass concentration of lithium in the retentate, x-dependent",
     )
     m.retentate_conc_mass_chlorine = Var(
-        m.x,
+        m.x_bar,
         initialize=6.81,
         units=units.kg / units.m**3,
         domain=NonNegativeReals,
         doc="Mass concentration of chlorine in the retentate, x-dependent",
     )
     m.permeate_flow_volume = Var(
-        m.x,
+        m.x_bar,
         initialize=0,
         units=units.m**3 / units.h,
         domain=NonNegativeReals,
         doc="Volumetric flow rate of the permeate, x-dependent",
     )
     m.permeate_conc_mass_lithium = Var(
-        m.x,
+        m.x_bar,
         initialize=0,
         units=units.kg / units.m**3,
         domain=NonNegativeReals,
         doc="Mass concentration of lithium in the permeate, x-dependent",
     )
     m.permeate_conc_mass_chlorine = Var(
-        m.x,
+        m.x_bar,
         initialize=0,
         units=units.kg / units.m**3,
         domain=NonNegativeReals,
         doc="Mass concentration of chlorine in the retentate, x-dependent",
     )
     m.osmotic_pressure = Var(
-        m.x,
+        m.x_bar,
         initialize=5,
         units=units.bar,
         domain=NonNegativeReals,
@@ -239,16 +242,16 @@ def build_model():
 
     ## dependent on z_hat and x_hat
     m.membrane_conc_mass_lithium = Var(
-        m.x,
-        m.z,
+        m.x_bar,
+        m.z_bar,
         initialize=1.7,
         units=units.kg / units.m**3,
         domain=NonNegativeReals,
         doc="Mass concentration of lithium in the membrane, x- and z-dependent",
     )
     m.membrane_conc_mass_chlorine = Var(
-        m.x,
-        m.z,
+        m.x_bar,
+        m.z_bar,
         initialize=8.7,
         units=units.kg / units.m**3,
         domain=NonNegativeReals,
@@ -258,26 +261,26 @@ def build_model():
     # define the (partial) derivative variables
     m.d_retentate_conc_mass_lithium_dx = DerivativeVar(
         m.retentate_conc_mass_lithium,
-        wrt=m.x,
-        units=units.kg / units.m**3 / units.m,
+        wrt=m.x_bar,
+        units=units.kg / units.m**3,
     )
     m.d_retentate_flow_volume_dx = DerivativeVar(
         m.retentate_flow_volume,
-        wrt=m.x,
-        units=units.m**3 / units.h / units.m,
+        wrt=m.x_bar,
+        units=units.m**3 / units.h,
     )
     m.d_membrane_conc_mass_lithium_dz = DerivativeVar(
         m.membrane_conc_mass_lithium,
-        wrt=m.z,
-        units=units.kg / units.m**3 / units.m,
+        wrt=m.z_bar,
+        units=units.kg / units.m**3,
     )
 
     # define the constraints
     ## mass balance constraints
     def _overall_mass_balance(m, x):
-        return m.d_retentate_flow_volume_dx[x] == (-m.volume_flux_water[x] * m.L)
+        return m.d_retentate_flow_volume_dx[x] == (-m.volume_flux_water[x] * m.L * m.w)
 
-    m.overall_mass_balance = Constraint(m.x, rule=_overall_mass_balance)
+    m.overall_mass_balance = Constraint(m.x_bar, rule=_overall_mass_balance)
 
     def _lithium_mass_balance(m, x):
         return (m.retentate_flow_volume[x] * m.d_retentate_conc_mass_lithium_dx[x]) == (
@@ -285,19 +288,19 @@ def build_model():
                 m.volume_flux_water[x] * m.retentate_conc_mass_lithium[x]
                 - m.mass_flux_lithium[x]
             )
-            * m.L
+            * m.L * m.w
         )
 
-    m.lithium_mass_balance = Constraint(m.x, rule=_lithium_mass_balance)
+    m.lithium_mass_balance = Constraint(m.x_bar, rule=_lithium_mass_balance)
 
     ## transport constraints
     def _geometric_flux_equation_overall(m, x):
         if x == 0:
             return Constraint.Skip
-        return m.permeate_flow_volume[x] == m.volume_flux_water[x] * x * units.m * m.L
+        return m.permeate_flow_volume[x] == m.volume_flux_water[x] * x * m.L * m.w
 
     m.geometric_flux_equation_overall = Constraint(
-        m.x, rule=_geometric_flux_equation_overall
+        m.x_bar, rule=_geometric_flux_equation_overall
     )
 
     def _geometric_flux_equation_lithium(m, x):
@@ -308,7 +311,7 @@ def build_model():
         )
 
     m.geometric_flux_equation_lithium = Constraint(
-        m.x, rule=_geometric_flux_equation_lithium
+        m.x_bar, rule=_geometric_flux_equation_lithium
     )
 
     def _lumped_water_flux(m, x):
@@ -316,7 +319,7 @@ def build_model():
             return Constraint.Skip
         return m.volume_flux_water[x] == (m.Lp * (m.dP - m.osmotic_pressure[x]))
 
-    m.lumped_water_flux = Constraint(m.x, rule=_lumped_water_flux)
+    m.lumped_water_flux = Constraint(m.x_bar, rule=_lumped_water_flux)
 
     def _lithium_flux_membrane(m, x, z):
         if z == 0:
@@ -328,11 +331,11 @@ def build_model():
                 * m.D_chlorine
                 * (m.z_lithium - m.z_chlorine)
                 / (m.D_lithium * m.z_lithium - m.D_chlorine * m.z_chlorine)
-            )
+            ) / m.l
             * m.d_membrane_conc_mass_lithium_dz[x, z]
         )
 
-    m.lithium_flux_membrane = Constraint(m.x, m.z, rule=_lithium_flux_membrane)
+    m.lithium_flux_membrane = Constraint(m.x_bar, m.z_bar, rule=_lithium_flux_membrane)
 
     def _chlorine_flux_membrane(m, x):
         return (
@@ -342,7 +345,7 @@ def build_model():
             / m.molar_mass_lithium
         )
 
-    m.chlorine_flux_membrane = Constraint(m.x, rule=_chlorine_flux_membrane)
+    m.chlorine_flux_membrane = Constraint(m.x_bar, rule=_chlorine_flux_membrane)
 
     ## other physical constaints
     def _osmotic_pressure_calculation(m, x):
@@ -373,7 +376,7 @@ def build_model():
         )
 
     m.osmotic_pressure_calcualation = Constraint(
-        m.x, rule=_osmotic_pressure_calculation
+        m.x_bar, rule=_osmotic_pressure_calculation
     )
 
     ## boundary conditions
@@ -381,34 +384,34 @@ def build_model():
         return m.retentate_conc_mass_lithium[x] == m.membrane_conc_mass_lithium[x, 0]
 
     m.retentate_membrane_interface_lithium = Constraint(
-        m.x, rule=_retentate_membrane_interface_lithium
+        m.x_bar, rule=_retentate_membrane_interface_lithium
     )
 
     def _retentate_membrane_interface_chlorine(m, x):
         return m.retentate_conc_mass_chlorine[x] == m.membrane_conc_mass_chlorine[x, 0]
 
     m.retentate_membrane_interface_chlorine = Constraint(
-        m.x, rule=_retentate_membrane_interface_chlorine
+        m.x_bar, rule=_retentate_membrane_interface_chlorine
     )
 
     def _membrane_permeate_interface_lithium(m, x):
         return (
             m.permeate_conc_mass_lithium[x]
-            == m.membrane_conc_mass_lithium[x, value(m.l)]
+            == m.membrane_conc_mass_lithium[x, 1]
         )
 
     m.membrane_permeate_interface_lithium = Constraint(
-        m.x, rule=_membrane_permeate_interface_lithium
+        m.x_bar, rule=_membrane_permeate_interface_lithium
     )
 
     def _membrane_permeate_interface_chlorine(m, x):
         return (
             m.permeate_conc_mass_chlorine[x]
-            == m.membrane_conc_mass_chlorine[x, value(m.l)]
+            == m.membrane_conc_mass_chlorine[x, 1]
         )
 
     m.membrane_permeate_interface_chlorine = Constraint(
-        m.x, rule=_membrane_permeate_interface_chlorine
+        m.x_bar, rule=_membrane_permeate_interface_chlorine
     )
 
     def _electroneutrality_retentate(m, x):
@@ -417,7 +420,7 @@ def build_model():
             + m.z_chlorine * m.retentate_conc_mass_chlorine[x] / m.molar_mass_chlorine
         )
 
-    m.electroneutrality_retentate = Constraint(m.x, rule=_electroneutrality_retentate)
+    m.electroneutrality_retentate = Constraint(m.x_bar, rule=_electroneutrality_retentate)
 
     def _electroneutrality_membrane(m, x, z):
         if z == 0:
@@ -428,7 +431,7 @@ def build_model():
         )
 
     m.electroneutrality_membrane = Constraint(
-        m.x, m.z, rule=_electroneutrality_membrane
+        m.x_bar, m.z_bar, rule=_electroneutrality_membrane
     )
 
     ## initial/final conditions
@@ -506,11 +509,11 @@ def build_model():
             )
         )
 
-    m.general_mass_balance_lithium = Constraint(m.x, rule=_general_mass_balance_lithium)
+    m.general_mass_balance_lithium = Constraint(m.x_bar, rule=_general_mass_balance_lithium)
 
     def _initial_d_retentate_conc_mass_lithium_dx(m):
         return m.d_retentate_conc_mass_lithium_dx[0] == (
-            0 * units.kg / units.m**3 / units.m
+            0 * units.kg / units.m**3
         )
 
     m.initial_d_retentate_conc_mass_lithium_dx = Constraint(
@@ -518,7 +521,7 @@ def build_model():
     )
 
     def _initial_d_retentate_flow_volume_dx(m):
-        return m.d_retentate_flow_volume_dx[0] == (0 * units.m**3 / units.h / units.m)
+        return m.d_retentate_flow_volume_dx[0] == (0 * units.m**3 / units.h)
 
     m._initial_d_retentate_flow_volume_dx = Constraint(
         rule=_initial_d_retentate_flow_volume_dx
@@ -530,13 +533,13 @@ def build_model():
 def discretize_model(m, NFEx, NFEz, discretization="col"):
     if discretization == "findif":
         discretizer_findif = TransformationFactory("dae.finite_difference")
-        discretizer_findif.apply_to(m, wrt=m.x, nfe=NFEx, scheme="FORWARD")
-        discretizer_findif.apply_to(m, wrt=m.z, nfe=NFEz, scheme="FORWARD")
+        discretizer_findif.apply_to(m, wrt=m.x_bar, nfe=NFEx, scheme="FORWARD")
+        discretizer_findif.apply_to(m, wrt=m.z_bar, nfe=NFEz, scheme="FORWARD")
 
     if discretization == "col":
         discretizer_col = TransformationFactory("dae.collocation")
-        discretizer_col.apply_to(m, wrt=m.x, nfe=NFEx, ncp=3, scheme="LAGRANGE-RADAU")
-        discretizer_col.apply_to(m, wrt=m.z, nfe=NFEz, ncp=3, scheme="LAGRANGE-RADAU")
+        discretizer_col.apply_to(m, wrt=m.x_bar, nfe=NFEx, ncp=3, scheme="LAGRANGE-RADAU")
+        discretizer_col.apply_to(m, wrt=m.z_bar, nfe=NFEz, ncp=3, scheme="LAGRANGE-RADAU")
 
 
 def solve_model(m):
@@ -561,15 +564,15 @@ def optimize(m):
 
 
 def plot_results(m):
-    x = []
+    x_plot = []
     conc_ret_lith = []
     conc_perm_lith = []
 
     water_flux = []
     lithium_flux = []
 
-    for x_val in m.x:
-        x.append(x_val)
+    for x_val in m.x_bar:
+        x_plot.append(x_val*value(m.w))
         conc_ret_lith.append(value(m.retentate_conc_mass_lithium[x_val]))
         conc_perm_lith.append(value(m.permeate_conc_mass_lithium[x_val]))
 
@@ -578,19 +581,19 @@ def plot_results(m):
 
     fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
-    ax1.plot(x, conc_ret_lith)
+    ax1.plot(x_plot, conc_ret_lith)
     ax1.set_xlabel("Membrane Length (m)")
     ax1.set_ylabel("Retentate-side Lithium Concentration (kg/m3)")
 
-    ax2.plot(x, conc_perm_lith)
+    ax2.plot(x_plot, conc_perm_lith)
     ax2.set_xlabel("Membrane Length (m)")
     ax2.set_ylabel("Permeate-side Lithium Concentration (kg/m3)")
 
-    ax3.plot(x, water_flux)
+    ax3.plot(x_plot, water_flux)
     ax3.set_xlabel("Membrane Length (m)")
     ax3.set_ylabel("Water Flux (m3/m2/h)")
 
-    ax4.plot(x, lithium_flux)
+    ax4.plot(x_plot, lithium_flux)
     ax4.set_xlabel("Membrane Length (m)")
     ax4.set_ylabel("Mass Flux of Lithium (kg/m2/h)")
 
