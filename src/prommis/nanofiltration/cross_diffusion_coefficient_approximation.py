207d64a11b11190bb3c50fdffbee871737f0359c
Molly Dougher <mdoughe6@nd.edu>
2025-03-11 21:37:11 -0400

    update concentration ranges; run black

--- src/prommis/nanofiltration/cross_diffusion_coefficient_approximation.py
+++ src/prommis/nanofiltration/cross_diffusion_coefficient_approximation.py
@@ -15,6 +15,7 @@ from pyomo.environ import (
     Var,
 )
 
+
 def main():
     # (D_11_df, D_12_df, D_21_df, D_22_df, c1, c2) = calculate_diffusion_coefficients()
     # plot_2D_diffusion_coefficients(D_11_df, D_12_df, D_21_df, D_22_df)
@@ -24,44 +25,51 @@ def main():
     plot_3D_with_regression()
 
 
-def calculate_D_11(z1,z2,z3,D1,D2,D3,c1,c2):
-    D_11 = ((z1*z3*D1*D3 - (z1**2)*D1*D3)*c1 + (z2*z3*D1*D3 - (z2**2)*D1*D2)*c2) / (
-        ((z1**2)*D1 - z1*z3*D3)*c1 + ((z2**2)*D2 - z2*z3*D3)*c2
-    )
+def calculate_D_11(z1, z2, z3, D1, D2, D3, c1, c2):
+    D_11 = (
+        (z1 * z3 * D1 * D3 - (z1**2) * D1 * D3) * c1
+        + (z2 * z3 * D1 * D3 - (z2**2) * D1 * D2) * c2
+    ) / (((z1**2) * D1 - z1 * z3 * D3) * c1 + ((z2**2) * D2 - z2 * z3 * D3) * c2)
     return D_11
 
-def calculate_D_12(z1,z2,z3,D1,D2,D3,c1,c2):
-    D_12 = ((z1*z2*D1*D2 - z1*z2*D1*D3)*c1) / (
-        ((z1**2)*D1 - z1*z3*D3)*c1 + ((z2**2)*D2 - z2*z3*D3)*c2
+
+def calculate_D_12(z1, z2, z3, D1, D2, D3, c1, c2):
+    D_12 = ((z1 * z2 * D1 * D2 - z1 * z2 * D1 * D3) * c1) / (
+        ((z1**2) * D1 - z1 * z3 * D3) * c1 + ((z2**2) * D2 - z2 * z3 * D3) * c2
     )
     return D_12
 
-def calculate_D_21(z1,z2,z3,D1,D2,D3,c1,c2):
-    D_21 = ((z1*z2*D1*D2 - z1*z2*D2*D3)*c2) / (
-        ((z1**2)*D1 - z1*z3*D3)*c1 + ((z2**2)*D2 - z2*z3*D3)*c2
+
+def calculate_D_21(z1, z2, z3, D1, D2, D3, c1, c2):
+    D_21 = ((z1 * z2 * D1 * D2 - z1 * z2 * D2 * D3) * c2) / (
+        ((z1**2) * D1 - z1 * z3 * D3) * c1 + ((z2**2) * D2 - z2 * z3 * D3) * c2
     )
     return D_21
 
-def calculate_D_22(z1,z2,z3,D1,D2,D3,c1,c2):
-    D_22 = ((z1*z2*D2*D3 - (z1**2)*D1*D2)*c1 + (z2*z3*D2*D3 - (z2**2)*D2*D3)*c2) / (
-        ((z1**2)*D1 - z1*z3*D3)*c1 + ((z2**2)*D2 - z2*z3*D3)*c2
-    )
+
+def calculate_D_22(z1, z2, z3, D1, D2, D3, c1, c2):
+    D_22 = (
+        (z1 * z2 * D2 * D3 - (z1**2) * D1 * D2) * c1
+        + (z2 * z3 * D2 * D3 - (z2**2) * D2 * D3) * c2
+    ) / (((z1**2) * D1 - z1 * z3 * D3) * c1 + ((z2**2) * D2 - z2 * z3 * D3) * c2)
     return D_22
 
+
 def set_concentration_ranges():
-    c1_vals = np.arange(0.1,10,0.1)
-    c2_vals = np.arange(0.1,10,0.1)
+    c1_vals = np.arange(0.1, 5, 0.1)
+    c2_vals = np.arange(4, 8, 0.1)
 
     return (c1_vals, c2_vals)
+
 
 def calculate_diffusion_coefficients():
     z1 = 1
     z2 = 2
     z3 = -1
 
-    D1 = 3.7e-6 # m2/h
-    D2 = 2.64e-6 # m2/h
-    D3 = 7.3e-6 # m2/h
+    D1 = 3.7e-6  # m2/h
+    D2 = 2.64e-6  # m2/h
+    D3 = 7.3e-6  # m2/h
 
     (c1_vals, c2_vals) = set_concentration_ranges()
 
@@ -82,10 +90,10 @@ def calculate_diffusion_coefficients():
 
     for c1 in c1_vals:
         for c2 in c2_vals:
-            D_11_vals.append((calculate_D_11(z1,z2,z3,D1,D2,D3,c1,c2)))
-            D_12_vals.append((calculate_D_12(z1,z2,z3,D1,D2,D3,c1,c2)))
-            D_21_vals.append((calculate_D_21(z1,z2,z3,D1,D2,D3,c1,c2)))
-            D_22_vals.append((calculate_D_22(z1,z2,z3,D1,D2,D3,c1,c2)))
+            D_11_vals.append((calculate_D_11(z1, z2, z3, D1, D2, D3, c1, c2)))
+            D_12_vals.append((calculate_D_12(z1, z2, z3, D1, D2, D3, c1, c2)))
+            D_21_vals.append((calculate_D_21(z1, z2, z3, D1, D2, D3, c1, c2)))
+            D_22_vals.append((calculate_D_22(z1, z2, z3, D1, D2, D3, c1, c2)))
         d11[f"{c1.round(1)}"] = D_11_vals
         d12[f"{c1.round(1)}"] = D_12_vals
         d21[f"{c1.round(1)}"] = D_21_vals
@@ -95,15 +103,16 @@ def calculate_diffusion_coefficients():
         D_21_vals = []
         D_22_vals = []
 
-    D_11_df = DataFrame(index=c2_list,data=d11)
-    D_12_df = DataFrame(index=c2_list,data=d12)
-    D_21_df = DataFrame(index=c2_list,data=d21)
-    D_22_df = DataFrame(index=c2_list,data=d22)
-    
+    D_11_df = DataFrame(index=c2_list, data=d11)
+    D_12_df = DataFrame(index=c2_list, data=d12)
+    D_21_df = DataFrame(index=c2_list, data=d21)
+    D_22_df = DataFrame(index=c2_list, data=d22)
+
     return (D_11_df, D_12_df, D_21_df, D_22_df, c1_vals, c2_vals)
 
+
 def plot_2D_diffusion_coefficients(D_11_df, D_12_df, D_21_df, D_22_df):
-    figs, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2,2)
+    figs, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
     sns.heatmap(
         ax=ax1,
         data=D_11_df,
@@ -150,35 +159,38 @@ def plot_2D_diffusion_coefficients(D_11_df, D_12_df, D_21_df, D_22_df):
 
     plt.show()
 
+
 def plot_3D_diffusion_coefficients():
-    ax = plt.figure().add_subplot(projection='3d')
+    ax = plt.figure().add_subplot(projection="3d")
 
     z1 = 1
     z2 = 2
     z3 = -1
 
-    D1 = 3.7e-6 # m2/h
-    D2 = 2.64e-6 # m2/h
-    D3 = 7.3e-6 # m2/h
+    D1 = 3.7e-6  # m2/h
+    D2 = 2.64e-6  # m2/h
+    D3 = 7.3e-6  # m2/h
 
     (c1_vals, c2_vals) = set_concentration_ranges()
 
     c1, c2 = np.meshgrid(c1_vals, c2_vals)
-    D_11 = ((z1*z3*D1*D3 - (z1**2)*D1*D3)*c1 + (z2*z3*D1*D3 - (z2**2)*D1*D2)*c2) / (
-        ((z1**2)*D1 - z1*z3*D3)*c1 + ((z2**2)*D2 - z2*z3*D3)*c2
-    )
+    D_11 = (
+        (z1 * z3 * D1 * D3 - (z1**2) * D1 * D3) * c1
+        + (z2 * z3 * D1 * D3 - (z2**2) * D1 * D2) * c2
+    ) / (((z1**2) * D1 - z1 * z3 * D3) * c1 + ((z2**2) * D2 - z2 * z3 * D3) * c2)
 
     ax.plot_surface(
         c1,
         c2,
         D_11,
-        cmap='mako',
+        cmap="mako",
     )
 
     ax.set_xlabel("Lithium Concentration (kg/m3)")
     ax.set_ylabel("Cobalt Concentration (kg/m3)")
 
     plt.show()
+
 
 def linear_regression(D_11_df):
     m = ConcreteModel()
@@ -190,20 +202,23 @@ def linear_regression(D_11_df):
     m.c1_data = Set(initialize=[float(c1) for c1 in D_11_df.columns])
     m.c2_data = Set(initialize=[float(c2) for c2 in D_11_df.index])
 
-    m.D_11_prediction = Var(m.c1_data, m.c2_data,initialize=1e-6)
+    m.D_11_prediction = Var(m.c1_data, m.c2_data, initialize=1e-6)
 
-    def D_11_calculation(m,c1,c2):
-        return m.D_11_prediction[c1,c2] == (m.beta_0 + m.beta_1*c1 + m.beta_2*c2)
+    def D_11_calculation(m, c1, c2):
+        return m.D_11_prediction[c1, c2] == (m.beta_0 + m.beta_1 * c1 + m.beta_2 * c2)
+
     m.model_eqn = Constraint(m.c1_data, m.c2_data, rule=D_11_calculation)
 
     residual = 0
     for c1 in D_11_df.columns:
         for c2 in D_11_df.index:
-            residual += (m.D_11_prediction[float(c1),float(c2)] - D_11_df.loc[c2][c1])**2
+            residual += (
+                m.D_11_prediction[float(c1), float(c2)] - D_11_df.loc[c2][c1]
+            ) ** 2
 
     m.objective = Objective(expr=residual)
 
-    solver = SolverFactory('ipopt')
+    solver = SolverFactory("ipopt")
     solver.solve(m, tee=True)
 
     m.beta_0.display()
@@ -212,14 +227,15 @@ def linear_regression(D_11_df):
 
     return (m.beta_0.value, m.beta_1.value, m.beta_2.value)
 
+
 def calculate_linearized_diffusion_coefficients(D_11_df):
     z1 = 1
     z2 = 2
     z3 = -1
 
-    D1 = 3.7e-6 # m2/h
-    D2 = 2.64e-6 # m2/h
-    D3 = 7.3e-6 # m2/h
+    D1 = 3.7e-6  # m2/h
+    D2 = 2.64e-6  # m2/h
+    D3 = 7.3e-6  # m2/h
 
     (c1_vals, c2_vals) = set_concentration_ranges()
 
@@ -242,7 +258,7 @@ def calculate_linearized_diffusion_coefficients(D_11_df):
 
     for c1 in c1_vals:
         for c2 in c2_vals:
-            D_11_vals.append(beta_0 + beta_1*c1 + beta_2*c2)
+            D_11_vals.append(beta_0 + beta_1 * c1 + beta_2 * c2)
             # D_12_vals.append((calculate_D_12(z1,z2,z3,D1,D2,D3,c1,c2)))
             # D_21_vals.append((calculate_D_21(z1,z2,z3,D1,D2,D3,c1,c2)))
             # D_22_vals.append((calculate_D_22(z1,z2,z3,D1,D2,D3,c1,c2)))
@@ -255,40 +271,44 @@ def calculate_linearized_diffusion_coefficients(D_11_df):
         # D_21_vals = []
         # D_22_vals = []
 
-    D_11_df_linearized = DataFrame(index=c2_list,data=d11)
+    D_11_df_linearized = DataFrame(index=c2_list, data=d11)
     # D_12_df = DataFrame(index=c2_list,data=d12)
     # D_21_df = DataFrame(index=c2_list,data=d21)
     # D_22_df = DataFrame(index=c2_list,data=d22)
-    
+
     # return (D_11_df, D_12_df, D_21_df, D_22_df, c1_vals, c2_vals)
     print(D_11_df_linearized)
     return D_11_df_linearized
 
+
 def plot_3D_with_regression():
-    ax = plt.figure().add_subplot(projection='3d')
+    ax = plt.figure().add_subplot(projection="3d")
 
     z1 = 1
     z2 = 2
     z3 = -1
 
-    D1 = 3.7e-6 # m2/h
-    D2 = 2.64e-6 # m2/h
-    D3 = 7.3e-6 # m2/h
+    D1 = 3.7e-6  # m2/h
+    D2 = 2.64e-6  # m2/h
+    D3 = 7.3e-6  # m2/h
 
     (c1_vals, c2_vals) = set_concentration_ranges()
 
     c1, c2 = np.meshgrid(c1_vals, c2_vals)
-    D_11 = ((z1*z3*D1*D3 - (z1**2)*D1*D3)*c1 + (z2*z3*D1*D3 - (z2**2)*D1*D2)*c2) / (
-        ((z1**2)*D1 - z1*z3*D3)*c1 + ((z2**2)*D2 - z2*z3*D3)*c2
-    )
+    D_11 = (
+        (z1 * z3 * D1 * D3 - (z1**2) * D1 * D3) * c1
+        + (z2 * z3 * D1 * D3 - (z2**2) * D1 * D2) * c2
+    ) / (((z1**2) * D1 - z1 * z3 * D3) * c1 + ((z2**2) * D2 - z2 * z3 * D3) * c2)
 
     ax.plot_surface(
         c1,
         c2,
         D_11,
-        cmap='mako',
+        cmap="mako",
     )
-    (D_11_df, D_12_df, D_21_df, D_22_df, c1_list, c2_list) = calculate_diffusion_coefficients()
+    (D_11_df, D_12_df, D_21_df, D_22_df, c1_list, c2_list) = (
+        calculate_diffusion_coefficients()
+    )
     linearized_D_11 = calculate_linearized_diffusion_coefficients(D_11_df)
     ax.plot_surface(
         c1,
@@ -300,6 +320,7 @@ def plot_3D_with_regression():
     ax.set_ylabel("Cobalt Concentration (kg/m3)")
 
     plt.show()
+
 
 if __name__ == "__main__":
     main()
