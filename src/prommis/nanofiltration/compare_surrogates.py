de6465fb1ce070cacc634d67ac0303c59e37f976
mollydougher <118477334+mollydougher@users.noreply.github.com>
2025-09-17 16:11:50 -0400

    ran black

--- src/prommis/nanofiltration/compare_surrogates.py
+++ src/prommis/nanofiltration/compare_surrogates.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def main():
 
     surrogate_model_file_dict = {
@@ -20,9 +21,9 @@ def main():
 
     c1_vals = []
     c2_vals = []
-    for c1 in np.arange(50,201,1):
+    for c1 in np.arange(50, 201, 1):
         c1_vals.append(c1)
-    for c2 in np.arange(50,201,1):
+    for c2 in np.arange(50, 201, 1):
         c2_vals.append(c2)
 
     linear_surrogate_vals = []
@@ -37,21 +38,25 @@ def main():
     percent_error_dict_linear = {}
     percent_error_dict_rbf = {}
 
-    for c1 in np.arange(50,201,1):
-        for c2 in np.arange(50,201,1):
-            linear_val = evaluate_linear_surrogate(c1,c2)
+    for c1 in np.arange(50, 201, 1):
+        for c2 in np.arange(50, 201, 1):
+            linear_val = evaluate_linear_surrogate(c1, c2)
             linear_surrogate_vals.append(linear_val)
 
-            rbf_val = evaluate_surrogate(c1, c2, -140, _surrogates_obj_D_11, "D_11_scaled")
+            rbf_val = evaluate_surrogate(
+                c1, c2, -140, _surrogates_obj_D_11, "D_11_scaled"
+            )
             RBF_surrogate_vals.append(rbf_val)
 
             actual_val = calculate_true_value(c1, c2)
             actual_vals.append(actual_val)
 
-            percent_error_val_linear = abs(linear_val-actual_val)/abs(actual_val)*100
+            percent_error_val_linear = (
+                abs(linear_val - actual_val) / abs(actual_val) * 100
+            )
             percent_error_vals_linear.append(percent_error_val_linear)
 
-            percent_error_val_rbf = abs(rbf_val-actual_val)/abs(actual_val)*100
+            percent_error_val_rbf = abs(rbf_val - actual_val) / abs(actual_val) * 100
             percent_error_vals_rbf.append(percent_error_val_rbf)
 
         linear_surrogate_vals_dict[f"{c1}"] = linear_surrogate_vals
@@ -62,15 +67,15 @@ def main():
 
         linear_surrogate_vals = []
         RBF_surrogate_vals = []
-        actual_vals= []
+        actual_vals = []
         percent_error_vals_linear = []
         percent_error_vals_rbf = []
-    
-    linear_surrogate_vals_df = DataFrame(index=c2_vals,data=linear_surrogate_vals_dict)
-    RBF_surrogate_vals_df = DataFrame(index=c2_vals,data=RBF_surrogate_vals_dict)
-    actual_vals_df = DataFrame(index=c2_vals,data=actual_vals_dict)
-    percent_error_df_linear = DataFrame(index=c2_vals,data=percent_error_dict_linear)
-    percent_error_df_rbf = DataFrame(index=c2_vals,data=percent_error_dict_rbf)
+
+    linear_surrogate_vals_df = DataFrame(index=c2_vals, data=linear_surrogate_vals_dict)
+    RBF_surrogate_vals_df = DataFrame(index=c2_vals, data=RBF_surrogate_vals_dict)
+    actual_vals_df = DataFrame(index=c2_vals, data=actual_vals_dict)
+    percent_error_df_linear = DataFrame(index=c2_vals, data=percent_error_dict_linear)
+    percent_error_df_rbf = DataFrame(index=c2_vals, data=percent_error_dict_rbf)
 
     # fig1, (ax1,ax2, ax3) = plt.subplots(1,3)
 
@@ -82,16 +87,17 @@ def main():
 
     # plt.show()
 
-    fig2, (ax4,ax5) = plt.subplots(1,2)
+    fig2, (ax4, ax5) = plt.subplots(1, 2)
     percent_error_plot_linear = ax4.pcolor(c1_vals, c2_vals, percent_error_df_linear)
     percent_error_plot_rbf = ax5.pcolor(c1_vals, c2_vals, percent_error_df_rbf)
 
-    ax4.plot([73],[108],'ms',markersize=7)
-    ax5.plot([73],[108],'ms',markersize=7)
+    ax4.plot([73], [108], "ms", markersize=7)
+    ax5.plot([73], [108], "ms", markersize=7)
 
-    fig2.colorbar(percent_error_plot_linear,ax=[ax4,ax5])
-    
+    fig2.colorbar(percent_error_plot_linear, ax=[ax4, ax5])
+
     plt.show()
+
 
 def evaluate_linear_surrogate(c1, c2):
     val = -4.33e-06 + -4.21e-09 * c1 + 5.10e-09 * c2
@@ -135,7 +141,6 @@ def calculate_true_value(c1, c2):
     ) / D_denom
 
     return D_11
-
 
 
 if __name__ == "__main__":
